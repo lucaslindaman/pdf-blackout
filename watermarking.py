1844804697a48b8ae4050ae9723c874ce0ee2f6c
lucaslindaman <58007873+lucaslindaman@users.noreply.github.com>
2020-03-10 11:02:46 -0400

    Update watermarking.py
    Added comments for file input locations.

--- watermarking.py
+++ watermarking.py
@@ -16,5 +16,4 @@ def create_watermark(input_pdf, output_pdf, watermark_pdf):
         pdf_writer.write(out)
 
 if __name__ == '__main__':
-    create_watermark(input_pdf = 'G:\Watermarking\in.pdf', output_pdf = 'G:\Watermarking\out.pdf', watermark_pdf = 'G:\Watermarking\wm.pdf')
-
+    create_watermark(input_pdf = 'INSERT INPUT FILE LOCATION HERE', output_pdf = 'INSERT OUTPUT LOCATION HERE', watermark_pdf = 'INSERT WATERMARK FILE LOCATION HERE')
