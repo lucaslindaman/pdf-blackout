a87cd04aa05deca681277c170993e8f2feff43e2
lucaslindaman <58007873+lucaslindaman@users.noreply.github.com>
2020-03-10 11:00:04 -0400

    Add files via upload

---
+++ watermarking.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+def create_watermark(input_pdf, output_pdf, watermark_pdf):
+    watermark_obj = PdfFileReader(watermark_pdf)
+    watermark_page = watermark_obj.getPage(0)
+
+    pdf_reader = PdfFileReader(input_pdf)
+    pdf_writer = PdfFileWriter()
+
+    for page in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page)
+        page.mergePage(watermark_page)
+        pdf_writer.addPage(page)
+
+    with open(output_pdf, 'wb') as out:
+        pdf_writer.write(out)
+
+if __name__ == '__main__':
+    create_watermark(input_pdf = 'G:\Watermarking\in.pdf', output_pdf = 'G:\Watermarking\out.pdf', watermark_pdf = 'G:\Watermarking\wm.pdf')
+
